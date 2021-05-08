4f22e3348f55310b41909fadb1f977c1ad8d2b2a
Leromac <leromac@hotmail.com>
2021-05-07 23:07:08 -0500

    Creacion clase CompraProductoPK para crear la llave compuesta de la tabla Compras_productos

---
+++ src/main/java/com/platzi/market/persistence/entity/ComprasProductoPK.java
@@ -0,0 +1,31 @@
+package com.platzi.market.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+public class ComprasProductoPK implements Serializable {
+
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_producto")
+    private Integer idProdcuto;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Integer getIdProdcuto() {
+        return idProdcuto;
+    }
+
+    public void setIdProdcuto(Integer idProdcuto) {
+        this.idProdcuto = idProdcuto;
+    }
+}
